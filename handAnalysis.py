8d3e617fd6c9c82743714f901d5583fdff734b62
Marcelo Hikrota <marcelohirota@gmail.com>
2022-01-28 18:46:29 +1030

    comments

--- handAnalysis.py
+++ handAnalysis.py
@@ -1,17 +1,32 @@
 def poker(hands):
+    # check the score given by the score function
     scores = [(i, score(hand)) for i, hand in enumerate(hands)]
+    # Check both analysis, the hand with shorter lenght of dictionary will win
     winner = sorted(scores , key=lambda x:x[1])[-1][0]
     return hands[winner]
     
 def score(hand):
+    # Given a weigh for each value through the position of the string
     values = '23456789TJQKA'
+    # Separating the value from the suit, and appending to a dictionary
+    # Keys of the dictionary will be the values of the cards
+    # Values of the dictionary will be how many of the same value
     rcounts = {values.find(r): ''.join(hand).count(r) for r, nsuit in hand}.items()
+    # Evaluates if the hand has cards of the same value or not, and sorting it
+    # by descending order
     score, values = zip(*sorted((cnt, rank) for rank, cnt in rcounts)[::-1])
+    # If the dictionary created has 5 keys and values (no card of the same values), it should be checked the suits of the cards
     if len(score) == 5:
+        # check if the cards have the same suit
         if values[0:2] == (12, 3): 
             values = (3, 2, 1, 0, 1)
+        # check the diference between the bigger number and smaller number, 
+        # if there is a diference of only four numbers between them, 
+        # the values are a sequence
         straight = values[0] - values[4] == 4 # same suit
+        # check if is the top end of the sequence
         flush = len({suit for nsuit, suit in hand}) == 1 # same suit
+        # assign a score depending on the hand
         score = ([(1,), (3,1,1,1)], [(3,1,1,2), (5,)])[flush][straight]
         
     return score, values
--- pokerProgram.py
+++ pokerProgram.py
@@ -2,13 +2,17 @@ from pathlib2 import Path
 import pandas as pd
 from handAnalysis import poker
 
+# prepare data to be load to the dataframe, adding ',' to  the empty spaces
 path = Path('poker-hands.txt')
 text = path.read_text()
 text = text.replace(' ', ',')
 path.write_text(text)
 
+
 df = pd.read_csv('poker-hands.txt', header=None)
 
+# dividing the dataframe in 2, first 5 components will be player 1 hand 
+# and the rest will be player 2 hand
 player1 = df.iloc[:, 0:5]
 player2 = df.iloc[:, 5:10]
 
@@ -17,14 +21,20 @@ play2_wins = 0
 check1=[]
 check2=[]
 checkResult=[]
+
+# checking each hand given and calling the analysis function
 for i in range(df.shape[0]):
+    # Appending the cards of each player into a list
     player1_hand = player1.loc[i, :].values.tolist()
     player2_hand = player2.loc[i, :].values.tolist()
+    # Calling the analysis function
     result = poker([player1_hand, player2_hand])
+    
     if result == player1_hand:
         play1_wins += 1
     else:
         play2_wins += 1
 
-print('Player 1 won: {}'.format(play1_wins))
-print('Player 2 won: {}'.format(play2_wins))
+# Printing the result
+print('Player 1 won: {} hands'.format(play1_wins))
+print('Player 2 won: {} hands'.format(play2_wins))
